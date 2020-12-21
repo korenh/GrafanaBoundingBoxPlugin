2f0d86df7549bd9afde53902820f7f9b45e5a0e4
korenh <korenx123@gmail.com>
2020-12-21 10:16:12 +0200

    working+trim

--- src/module.tsx
+++ src/module.tsx
@@ -1,5 +1,34 @@
 import { PanelPlugin } from '@grafana/data';
 import { SimpleOptions } from './types';
-import { Panel } from './panel';
+import React, { PureComponent } from 'react';
+import { PanelProps } from '@grafana/data';
+const Annotation = require('react-image-annotation').Annotation;
+const RectangleSelector = require('react-image-annotation/lib/selectors').RectangleSelector;
 
+export class Panel extends PureComponent<PanelProps> {
+  
+    render() {
+      return <div>
+      <Annotation
+      src="https://source.unsplash.com/random/800x600"
+      annotations={[{
+        "geometry":{
+          "type":RectangleSelector.TYPE,
+          "x":0,
+          "y":0,
+          "width": 50,
+          "height": 50,
+        },
+        "data":{
+          "text":"korenk",
+          "id":"korenk"
+        }
+      }]}
+      value={{}}
+      disableOverlay={true}
+    /></div>;
+    }
+  }
+  
+  
 export const plugin = new PanelPlugin<SimpleOptions>(Panel);
